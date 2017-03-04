010737a79456ee0a0e3ac75f8bfbf034f80003c1
Jérémie Kaszub <Jrmie Kaszub>
2017-03-04 02:53:03 +0100

    (feat): implement fliping panel & design buttons

--- src/AppBundle/Resources/scripts/index.js
+++ src/AppBundle/Resources/scripts/index.js
@@ -1,1 +1,12 @@
-console.log('test');
+//Buttons class addition to allow flip
+
+$(document).ready(function() {
+
+    var $bioButton = $("#jk-home-bio-button");
+    var $flipingContainer = $("#jk-home-bio-container");
+
+    $bioButton.click(function () {
+        console.log('ftw');
+        $flipingContainer.toggleClass("active");
+    });
+});
