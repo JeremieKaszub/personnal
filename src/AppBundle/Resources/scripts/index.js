8971c483edcd22fb3db3b4950561005ea241dd1a
Jérémie Kaszub <Jrmie Kaszub>
2017-03-05 15:01:08 +0100

    feat: implement basic score block

--- src/AppBundle/Resources/scripts/index.js
+++ src/AppBundle/Resources/scripts/index.js
@@ -1,12 +1,26 @@
-//Buttons class addition to allow flip
-
 $(document).ready(function() {
 
+    //Buttons class addition to allow flip
     var $bioButton = $("#jk-home-bio-button");
     var $flipingContainer = $("#jk-home-bio-container");
+    var $bioContainer = $("#jk-home-bio-container-content");
+    var $bioDescription = $("#jk-home-bio-container-description");
 
+    // Class addition
     $bioButton.click(function () {
         console.log('ftw');
         $flipingContainer.toggleClass("active");
+        $bioContainer.toggleClass("active");
+        $bioDescription.toggleClass("active");
+    });
+
+    //Buttons class addition to fill scores
+    var $scoreItem = $("[data-jk-score-item]");
+    var $scroreButton = $("#jk-home-skills-button");
+
+    // Class addition
+    $scroreButton.click(function () {
+        console.log('ftw');
+        $scoreItem.toggleClass("active");
     });
 });
